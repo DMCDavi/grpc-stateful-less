c344abcd891c5c81d6c2736253c3043c0cb63146
Davi Costa <davi.machado.costa@hotmail.com>
2022-04-04 20:33:33 -0300

    Update greeter_server.py

--- examples/python/helloworld/greeter_server.py
+++ examples/python/helloworld/greeter_server.py
@@ -16,55 +16,51 @@
 from concurrent import futures
 from email import message
 import logging
-import time
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
 from vinte_um import Jogador, VinteUm
-#from vinte_um import Jogador, VinteUm
 game = None
 
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
-    #global game
+
     def Login(self, request, context):
         global game
         auth_token = request.username + '_' + request.password
   
-        if(game == None):
+        if(game == None or game.endGame == True):
             createGame()
         player = game.criarJogador(auth_token)
         player.criarMao(game.baralho)
 
-        return helloworld_pb2.LoginReply(message='Você fez login com sucesso.', auth_token=auth_token)
+        return helloworld_pb2.LoginReply(message=str(player.mao), auth_token=auth_token)
 
 
     def TurnAction(self, request, context):
         global game
-        
+        message = ""
         jogador = self.VerifyAuth(request.auth_token)
-        print(jogador.mao)
+
         if(request.dig == "S" or request.dig == "s"):
             jogador.cavarCarta(game.baralho)
+            message = jogador.verificaSoma()
         else:
             jogador.jogando = False
+            message = "Voce encerrou sua partida, espere os outros jogadores"
         changeTurn()
         
-        return helloworld_pb2.HelloReply(message=str(jogador.jogando))
+        return helloworld_pb2.TurnReply(playing=str(jogador.jogando), cards=str(jogador.mao), message=message)
     
     def VerifyAuth(self, auth_token):
         global game
 
         for jogador in game.jogadores:
-          
-            if(jogador.auth_token == auth_token):
-            
-                
+            if(jogador.auth_token == auth_token):                
                 return jogador
         return None
  
     def VerifyTurn(self,request, context):
-        
         while(not game.endGame):
             continue
         message = self.ShowWinner()
@@ -76,13 +72,10 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
         som = 0
         for jogador in game.jogadores:
             if(jogador.soma == som):
-                print(jogador.soma)
                 jog.append(jogador)
                 som = jogador.soma
             elif(jogador.soma > som and jogador.soma <= 21):
-               
                 jog.clear()
-                
                 jog.append(jogador)
                 som = jogador.soma
             
@@ -115,12 +108,7 @@ def changeTurn():
         
 def createGame():
     global game
-    print("Bem vindo ao jogo VINTE E UM!")
-    time.sleep(2)
-    print("Embaralhando as cartas...")
-    time.sleep(2)
-    print("Distribuindo as cartas...")
-    time.sleep(2)
+    game = None
     game = VinteUm()
     game.criarBaralho()
     
@@ -131,7 +119,6 @@ def serve():
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    #createGame()
     server.wait_for_termination()
 
 
